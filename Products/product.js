03a0915851da558095dbec475d0b25460e210a3f
jolly <likhithpindi17@gmail.com>
2024-04-10 15:08:04 +0100

    added driks pages

--- Products/product.js
+++ Products/product.js
@@ -2,6 +2,10 @@ let sideDrinks = document.querySelector(".for-drinks");
 let content = document.querySelector(".iframes");
 let disply_drinks = document.querySelector(".main-items");
 let arrow = document.getElementById("arrow");
+let nav = document.querySelector("#nav");
+let allTypes = document.querySelectorAll("#add-drinks");
+let src_water = "../water/water.html";
+console.log(allTypes);
 let s = "deg";
 let flag = true;
 
@@ -16,5 +20,20 @@ let handleContent = function (e) {
     flag = !flag;
   }
 };
-
 sideDrinks.addEventListener("click", handleContent);
+
+let handlePage = function (items) {
+  if (items.innerHTML === "Water") {
+    nav.setAttribute("src", "../water/water.html");
+  } else if (items.innerHTML === "Monster") {
+    nav.setAttribute("src", "../Monster/monster.html");
+  } else if (items.innerHTML === "Lucozade") {
+    nav.setAttribute("src", "../Lucozade/lucozade.html");
+  } else if (items.innerHTML === "Vit Hit") {
+    nav.setAttribute("src", "../Vit hit/vit hit.html");
+  }
+};
+
+allTypes.forEach((items) => {
+  items.addEventListener("click", () => handlePage(items));
+});
